268ee55002992300b788cd142f34d3561214f990
huaishi <huaishi@2dfire.com>
2018-05-23 08:11:58 +0800

    first commit

---
+++ src/main/java/com/shengsiyuan/netty/first/TestHttpServerHandler.java
@@ -0,0 +1,23 @@
+package com.shengsiyuan.netty.first;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author huaishi
+ */
+public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+        ByteBuf content = Unpooled.copiedBuffer("Hello world", CharsetUtil.UTF_8);
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plain");
+        response.headers().set(HttpHeaderNames.CONTENT_LENGTH,content.readableBytes());
+
+        ctx.writeAndFlush(response);
+    }
+}
---
+++ src/main/java/com/shengsiyuan/netty/first/TestServerInitializer.java
@@ -0,0 +1,21 @@
+package com.shengsiyuan.netty.first;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+
+/**
+ * @author huaishi
+ */
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast("httpServerCodec", new HttpServerCodec());
+        pipeline.addLast("testHttpServerHandler", new TestHttpServerHandler());
+    }
+}
